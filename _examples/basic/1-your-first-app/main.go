f717c3746b6bfebcff32dea5bf5b6f93ac6dded1
Prema Roman <pdamo24@gmail.com>
2023-02-01 08:47:44 -0500

    Revert "minor edits"
    This reverts commit 907387ddf4b04f00ea1a33ef6e288d583d1a0481.

--- _examples/basic/1-your-first-app/main.go
+++ _examples/basic/1-your-first-app/main.go
@@ -107,7 +107,7 @@ func createPublisher() message.Publisher {
 
 // createSubscriber is a helper function similar to the previous one, but in this case it creates a Subscriber.
 func createSubscriber(consumerGroup string) message.Subscriber {
-	subscriber, err := ensign.NewSubscriber(
+	ensignSubscriber, err := ensign.NewSubscriber(
 		ensign.SubscriberConfig{
 			Unmarshaler: marshaler,
 		},
@@ -117,7 +117,7 @@ func createSubscriber(consumerGroup string) message.Subscriber {
 		panic(err)
 	}
 
-	return subscriber
+	return ensignSubscriber
 }
 
 // simulateEvents produces events that will be later consumed.
