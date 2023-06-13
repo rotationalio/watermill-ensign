a1cfe7f3ab6d087cf1f0e143ed5fcc283b8f0f68
Benjamin Bengfort <benjamin@bengfort.com>
2023-06-12 21:33:00 -0500

    fix marshaling tests

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/ThreeDotsLabs/watermill v1.2.0
 	github.com/google/uuid v1.3.0
 	github.com/pkg/errors v0.9.1
-	github.com/rotationalio/go-ensign v0.7.0
+	github.com/rotationalio/go-ensign v0.7.1-0.20230612220855-8f106c3c6947
 	github.com/stretchr/testify v1.8.4
 	google.golang.org/protobuf v1.30.0
 )
@@ -24,8 +24,8 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/text v0.10.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230530153820-e85fd2cbaebc // indirect
 	google.golang.org/grpc v1.55.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- pkg/ensign/marshaler.go
+++ pkg/ensign/marshaler.go
@@ -1,6 +1,7 @@
 package ensign
 
 import (
+	"encoding/hex"
 	"fmt"
 	"strconv"
 	"time"
@@ -9,26 +10,39 @@ import (
 	api "github.com/rotationalio/go-ensign"
 	pb "github.com/rotationalio/go-ensign/api/v1beta1"
 	mime "github.com/rotationalio/go-ensign/mimetype/v1beta1"
+	region "github.com/rotationalio/go-ensign/region/v1beta1"
 )
 
 // Header keys that the ensign marshaler expects to find in message metadata in order
 // to convert the Waterfall message into an Ensign event.
 const (
-	IDKey            = "event_id"
-	TopicIDKey       = "topic_id"
-	MIMEKey          = "mimetype"
-	TypeNameKey      = "type_name"
-	TypeVersionKey   = "type_version"
-	KeyKey           = "key"
-	EncAlgKey        = "encryption_algorithm"
-	EncKeyIDKey      = "encryption_key_id"
-	CompressAlgKey   = "compression_algorithm"
-	RegionNameKey    = "region_name"
-	PubClientIDKey   = "publisher_client_id"
-	PubIPAddrKey     = "publisher_ipaddr"
-	WatermillUUIDKey = "watermill_uuiid"
-	CreatedKey       = "created"
-	CommittedKey     = "committed"
+	// Event Data
+	MIMEKey        = "mimetype"
+	TypeNameKey    = "type_name"
+	TypeVersionKey = "type_version"
+	CreatedKey     = "created"
+
+	// Watermill Parity
+	WatermillUUIDKey = "watermill_uuid"
+
+	// Event Info
+	IDKey                 = "event_id"
+	TopicIDKey            = "topic_id"
+	OffsetKey             = "offset"
+	EpochKey              = "epoch"
+	RegionKey             = "region"
+	PublisherIDKey        = "publisher_id"
+	PublisherIPAddrKey    = "publisher_ipaddr"
+	PublisherClientIDKey  = "publisher_client_id"
+	PublisherUserAgentKey = "publisher_user_agent"
+	KeyKey                = "key"
+	ShardKey              = "shard"
+	EncAlgKey             = "encryption_algorithm"
+	EncKeyIDKey           = "encryption_public_key_id"
+	EncSigKey             = "encryption_signature"
+	CompressAlgKey        = "compression_algorithm"
+	CompressLevelKey      = "compression_level"
+	CommittedKey          = "committed"
 )
 
 // The number of metadata keys available in Ensign (to alloc metadata)
@@ -36,7 +50,9 @@ const nEnsignKeys = 15
 
 // Reserved metadata keys that cannot be in a message for serialization.
 var reserved = []string{
-	IDKey, TopicIDKey, CommittedKey, WatermillUUIDKey,
+	IDKey, TopicIDKey, OffsetKey, EpochKey, WatermillUUIDKey,
+	RegionKey, PublisherIDKey, PublisherIPAddrKey,
+	EncAlgKey, EncSigKey, ShardKey, CommittedKey,
 }
 
 // Marshaler transforms a Waterfall Message into an Ensign client library Event.
@@ -75,7 +91,11 @@ func (e EventMarshaler) Marshal(topic string, msg *message.Message) (event *api.
 	// TODO: how to add topic ID to the event from the topic string (or validate it)?
 	event = &api.Event{
 		Data:     msg.Payload,
-		Metadata: api.Metadata(msg.Metadata),
+		Metadata: make(api.Metadata, len(msg.Metadata)),
+	}
+
+	for key, val := range msg.Metadata {
+		event.Metadata[key] = val
 	}
 
 	event.Metadata[WatermillUUIDKey] = msg.UUID
@@ -92,11 +112,9 @@ func (e EventMarshaler) Marshal(topic string, msg *message.Message) (event *api.
 		}
 
 		if vers := msg.Metadata.Get(TypeVersionKey); vers != "" {
-			var version uint64
-			if version, err = strconv.ParseUint(vers, 10, 32); err != nil {
-				return nil, fmt.Errorf("could not parse type version: %w", err)
+			if err = event.Type.ParseSemver(vers); err != nil {
+				return nil, fmt.Errorf("could not parse metadata type version: %w", err)
 			}
-			event.Type.MajorVersion = uint32(version)
 		}
 	}
 
@@ -114,17 +132,57 @@ func (e EventMarshaler) Marshal(topic string, msg *message.Message) (event *api.
 func (e EventMarshaler) Unmarshal(event *api.Event) (*message.Message, error) {
 	// Create metadata from ensign event headers
 	metadata := make(message.Metadata, nEnsignKeys+len(event.Metadata))
-	for key, val := range event.Metadata {
-		metadata.Set(key, val)
-	}
-
-	// Set the mimetype into the message metadata
+	metadata.Set(IDKey, event.ID())
+	metadata.Set(TopicIDKey, event.TopicID())
 	metadata.Set(MIMEKey, event.Mimetype.MimeType())
 
 	// Set the event type with semantic version into the message metadata
 	if event.Type != nil {
 		metadata.Set(TypeNameKey, event.Type.Name)
-		metadata.Set(TypeVersionKey, fmt.Sprintf("%d.%d.%d", event.Type.MajorVersion, event.Type.MinorVersion, event.Type.PatchVersion))
+		metadata.Set(TypeVersionKey, event.Type.Semver())
+	}
+
+	info := event.Info()
+	if info != nil {
+
+		metadata.Set(OffsetKey, strconv.FormatUint(info.Offset, 10))
+		metadata.Set(EpochKey, strconv.FormatUint(info.Epoch, 10))
+
+		if info.Region != region.Region_UNKNOWN {
+			metadata.Set(RegionKey, info.Region.String())
+		}
+
+		if info.Publisher != nil && info.Publisher.PublisherId != "" {
+			metadata.Set(PublisherIDKey, info.Publisher.PublisherId)
+			metadata.Set(PublisherIPAddrKey, info.Publisher.Ipaddr)
+
+			if info.Publisher.ClientId != "" {
+				metadata.Set(PublisherClientIDKey, info.Publisher.ClientId)
+			}
+
+			if info.Publisher.UserAgent != "" {
+				metadata.Set(PublisherUserAgentKey, info.Publisher.UserAgent)
+			}
+		}
+
+		if len(info.Key) > 0 {
+			// TODO: should we add base64 encoding to the key?
+			metadata.Set(KeyKey, string(info.Key))
+			metadata.Set(ShardKey, strconv.FormatUint(info.Shard, 16))
+		}
+
+		if info.Encryption != nil {
+			metadata.Set(EncAlgKey, info.Encryption.EncryptionAlgorithm.String())
+			metadata.Set(EncKeyIDKey, info.Encryption.PublicKeyId)
+			metadata.Set(EncSigKey, hex.EncodeToString(info.Encryption.Signature))
+		}
+
+		if info.Compression != nil {
+			metadata.Set(CompressAlgKey, info.Compression.Algorithm.String())
+			if info.Compression.Level > 0 {
+				metadata.Set(CompressLevelKey, strconv.FormatInt(info.Compression.Level, 10))
+			}
+		}
 	}
 
 	// Set the created and committed timestamps
@@ -136,7 +194,13 @@ func (e EventMarshaler) Unmarshal(event *api.Event) (*message.Message, error) {
 		metadata.Set(CommittedKey, event.Committed().Format(time.RFC3339Nano))
 	}
 
-	// Attempt to determine the messasge UUID
+	// Set the user metadata back onto the message.
+	// NOTE: user keys will override ensign metadata keys that are not reserved.
+	for key, val := range event.Metadata {
+		metadata.Set(key, val)
+	}
+
+	// Attempt to determine the message UUID
 	var ok bool
 	var uuid string
 	if uuid, ok = event.Metadata[WatermillUUIDKey]; !ok {
@@ -147,5 +211,7 @@ func (e EventMarshaler) Unmarshal(event *api.Event) (*message.Message, error) {
 
 	msg := message.NewMessage(uuid, event.Data)
 	msg.Metadata = metadata
+	delete(msg.Metadata, WatermillUUIDKey)
+
 	return msg, nil
 }
